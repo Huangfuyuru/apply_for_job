562526e1eece93ee620ae77bce0ea685d52942cc
huangfuyuru <864134710@qq.com>
2020-01-04 18:14:31 +0800

    nex

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express'),
+      app = express(),
+      notes = require('./routes/notes');
+
+//解决跨域
+app.all('*',function(req,res,next){
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
+        res.header('Access-Control-Allow-Methods', '*');
+        res.header('Content-Type', 'application/json;charset=utf-8');
+        next();
+});
+
+app.use('/notes',notes);
+app.listen(3001);
